0b607360ef8ad8ddfbb9382d8dd194627c09210c
grekovpavel019 <grekovpavel065@gmail.com>
2026-09-03 19:49:29 +0300

    feature: initialize simple server

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log(123);
+import express, { type Express, type Request, type Response} from "express"
+
+const app: Express = express();
+
+app.post("/code", (req: Request, res: Response) => {
+    console.log("123");
+});
+
+app.listen(3000, "127.0.0.1", () => {
+    console.log("Сервер запущен")
+})
--- frontend/src/pages/CodePage/CodePage.tsx
+++ frontend/src/pages/CodePage/CodePage.tsx
@@ -11,7 +11,7 @@ const CodePage: FC = (): React.JSX.Element => {
 
     const [code, setCode] = useState<string>("");
 
-    const logCode: MouseEventHandler<HTMLButtonElement> = () => {
+    const sendCode: MouseEventHandler<HTMLButtonElement> = () => {
         console.log(code);
     }
 
@@ -34,7 +34,7 @@ const CodePage: FC = (): React.JSX.Element => {
 
             <div className={styles.codeActionsArea}>
                 <CodeActions 
-                    handleClick={logCode}
+                    handleClick={sendCode}
                 />
             </div>
         </main>
