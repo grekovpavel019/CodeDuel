17e0ef406f7ef93cc652e19141a8d3594a6bcf32
grekovpavel019 <grekovpavel065@gmail.com>
2026-09-11 12:49:38 +0300

    feature: start create login form

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,20 @@
 import React, { type FC } from "react";
 
+import styles from "./LoginPage.module.scss"
+
 const LoginPage: FC = (): React.JSX.Element => {
     return (
-        <div>
-            Страница логина
+        <div className={styles.loginWrapper}>
+
+            <div className={styles.loginContainer}>
+
+                {/* вынести в отдельный компонент */}
+                <form className={styles.form} action="">
+                    <label htmlFor="">Ввод</label>
+                    <input type="text" />
+                </form>
+            </div>
+
         </div>
     );
 };
