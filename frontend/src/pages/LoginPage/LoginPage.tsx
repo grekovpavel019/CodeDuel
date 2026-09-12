bf51b6a0af86275f66747b989d2af49964f85d8a
grekovpavel019 <grekovpavel065@gmail.com>
2026-09-12 18:22:01 +0300

    feature: create login page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,43 @@
 import React, { type FC } from "react";
 
+import Button from "@shared/components/button";
+
 import styles from "./LoginPage.module.scss"
 
 const LoginPage: FC = (): React.JSX.Element => {
     return (
-        <div className={styles.loginWrapper}>
+        <main className={styles.login}>
 
-            <div className={styles.loginContainer}>
+            <div className={styles.container}>
+                
+                <div className={styles.content}>
 
-                {/* вынести в отдельный компонент */}
-                <form className={styles.form} action="">
-                    <label htmlFor="">Ввод</label>
-                    <input type="text" />
-                </form>
+                    <div className={styles.heading}>
+                        <h1 className={styles.logo}>CodeDuel</h1>
+                        <h2 className={styles.subLogo}>Войти</h2>
+                    </div>
+
+                    <form className={styles.form} action="">
+                        <div className={styles.field}>
+                            <input id="login" type="text" placeholder=" "/>
+                            <label htmlFor="password">Логин</label>
+                        </div>
+
+                        <div className={styles.field}>
+                            <input id="password" type="password" placeholder=" "/>
+                            <label htmlFor="password">Пароль</label>
+                        </div>
+
+                        <div className={styles.buttonArea}>
+                            <Button format="primary">Войти</Button>
+                        </div>
+                    </form>
+
+                </div>
+
             </div>
 
-        </div>
+        </main>
     );
 };
 
